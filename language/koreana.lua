2f3871b1cc1c4803c80cff5285ccd3f540e3fa88
PixVoxel <wnsdl1247@gmail.com>
2026-03-01 03:24:12 +0900

    Updated Translation
    - with ver bump

--- language/koreana.lua
+++ language/koreana.lua
@@ -5,6 +5,20 @@ return {
             pickup = "선택 창을 닫습니다.",
             description = "<r>지휘의 유물로 생성된 상자의 선택 창을 닫습니다.</c> <b>상자는 소모되지 않습니다.</c>"
         },
-        keycard = { description = "특정 스테이지의 <b>잠긴 문</c>을 열 수 있습니다." },
+        keycard = { description = "특정 스테이지의 <b>잠긴 문</c>을 열 수 있습니다. 사용 시 소모됩니다." },
+        whiteUndershirt = { description = "<g>방어력</c>이 <g>3<c_stack>(중첩당 +3)</c> 증가합니다." },
+        smallEnigma = { description = "장비의 재사용 대기시간이 <b>5%<c_stack>(중첩당 +5%)</c> 감소합니다." },
+        strangeBattery = { description = "<y>연쇄 번개</c>를 발사하여 적에게 <y>33%의 피해</c>를 입힙니다." },
+        playerScrap1 = { description = "진짜?" },
+        playerScrap2 = { description = "아무런 쓸모가 없다." },
+        playerScrap3 = { description = "무쓸모." },
+        playerScrap4 = { description = "어쩌다가?" },
+        jellySalad = { description = "<b>스킬 재사용 대기시간이 1초<c_stack>(중첩당 +0.5초)</c> 감소합니다. 재사용 대기시간은 <b>0.5초</c> 이하로 내려갈 수 없습니다." },
+        marinatedLemurian = { description = "<y>피해량을 20%<c_stack>(중첩당 +10%)</c>만큼 증가시킵니다." },
+        golemBeer = { description = "획득 시 <g>최대 체력의 50%만큼 보호막</c>을 얻고 <b>방어력이 40<c_stack>(중첩당 +10)</c> 증가합니다." },
+        friedEyeball = { description = "주변 적들의 <b>이동 속도를 37.5%만큼</c>, <y>공격 속도를 25%만큼</c> 감소시킵니다<c_stack>(중첩당 범위 증가)</c>." },
+        saturnianSteak = { description = "<g>체력 재생</c>이 증가하여 1초마다 <g>0.6<c_stack>(중첩당 +0.6)<g>의 체력</c>을 회복하고, <b>이동 속도</c>를 <b>2<c_stack>(중첩당 +2)</c>만큼 증가시킵니다." },
+        diosFriendUsed = { description = "아무것도 하지 않습니다." },
+        timeKeepersSecretUsed = { description = "아무것도 하지 않습니다." }
     },
 }
